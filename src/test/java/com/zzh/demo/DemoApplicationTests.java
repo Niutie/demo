5faaca231d3cc7366a0f25bae63b137c7b2061a0
Your Name <you@example.com>
2018-10-16 14:49:15 +0800

    update

--- src/test/java/com/zzh/demo/DemoApplicationTests.java
+++ src/test/java/com/zzh/demo/DemoApplicationTests.java
@@ -1,9 +1,18 @@
 package com.zzh.demo;
 
+import com.zzh.demo.Entity.User;
 import org.junit.Test;
 import org.junit.runner.RunWith;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.jdbc.core.JdbcTemplate;
+import org.springframework.jdbc.core.RowMapper;
 import org.springframework.test.context.junit4.SpringRunner;
+
+import javax.annotation.Resource;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.util.List;
+
 
 @RunWith(SpringRunner.class)
 @SpringBootTest
@@ -13,4 +22,29 @@ public class DemoApplicationTests {
     public void contextLoads() {
     }
 
+    @Resource
+    private JdbcTemplate jdbcTemplate;
+
+    @Test
+    public void mySqlTest(){
+        String sql = "select id,name,password from user";
+        List<User> userList =
+                (List<User>) jdbcTemplate.query(sql, new RowMapper<User>(){
+                    @Override
+                    public User mapRow(ResultSet rs, int rowNum) throws SQLException {
+                        User user = new User();
+                        user.setId(rs.getString("id"));
+                        user.setName(rs.getString("name"));
+                        user.setPassword(rs.getString("password"));
+                        return user;
+                    }
+                });
+        System.out.println("查询成功：");
+        for(User user:userList){
+            System.out.println("【id】: " + user.getId() + "；【name】: " + user.getName());
+        }
+
+    }
 }
+
+
