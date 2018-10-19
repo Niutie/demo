5215d68e4812bb6038120169aa1410980d780ad8
Your Name <you@example.com>
2018-10-19 16:52:57 +0800

    update

---
+++ src/main/java/com/zzh/demo/quartz/SendMailQuartz.java
@@ -0,0 +1,17 @@
+package com.zzh.demo.quartz;
+
+import org.apache.logging.log4j.LogManager;
+import org.apache.logging.log4j.Logger;
+import org.springframework.beans.factory.annotation.Configurable;
+import org.springframework.scheduling.annotation.EnableScheduling;
+import org.springframework.scheduling.annotation.Scheduled;
+import org.springframework.stereotype.Component;
+
+@Component
+@Configurable
+@EnableScheduling
+public class SendMailQuartz {
+
+    private  static final Logger logger = LogManager.getLogger(SendMailQuartz.class);
+
+}
